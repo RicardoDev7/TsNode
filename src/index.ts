2b37f958dac68c78419d73ab80a5e6ebd1b20768
Ricardo Metzger <ricardo7.dev@gmail.com>
2025-12-26 18:57:14 -0500

    feat: metodos genericos en TS

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,14 @@
-import { Hero } from "./classes/Hero";
+import { genericFunction, printObject, genericFunctionArrow } from "./generics/generics";
 
-const ironMan = new Hero('Iron man', 2, 30);
-console.log(ironMan);
-console.log(ironMan.power);
+printObject('Hola');
+printObject(1234);
+printObject(['Ricardo', 'Alvaro', 'Rosa']);
+printObject([1, 2, 3]);
+printObject(50.50);
+printObject({a:'1', b:'2', c:3});
+
+console.log(genericFunction<Number>(123));
+console.log(genericFunction<Date>(new Date()).getDate());
+console.log(genericFunction<String>('Ricardo Metzger').toUpperCase());
+
+console.log(genericFunctionArrow('ABC'));
