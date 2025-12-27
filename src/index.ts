d2332c53e426854bb65ecd75a0f0866928eb84b6
Ricardo Metzger <ricardo7.dev@gmail.com>
2025-12-26 19:04:12 -0500

    feat: uso de genericos

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,8 @@
 import { genericFunction, printObject, genericFunctionArrow } from "./generics/generics";
+import { Hero } from "./interfaces/hero";
+import { Villain } from "./interfaces/villain";
 
+/*
 printObject('Hola');
 printObject(1234);
 printObject(['Ricardo', 'Alvaro', 'Rosa']);
@@ -12,3 +15,13 @@ console.log(genericFunction<Date>(new Date()).getDate());
 console.log(genericFunction<String>('Ricardo Metzger').toUpperCase());
 
 console.log(genericFunctionArrow('ABC'));
+*/
+
+const deadpool = {
+    name: 'Deadpool',
+    realName: 'Wade Winston Wilson',
+    dangerLevel: 130
+}
+
+console.log(genericFunctionArrow<Hero>(deadpool).realName);
+console.log(genericFunctionArrow<Villain>(deadpool).dangerLevel);
